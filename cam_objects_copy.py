78e31caf4b997c2601bb0f9fc37c02ffad3cba46
vigi4 <vigi4@mail.ru>
2021-03-03 22:09:33 +0300

    template

---
+++ cam_objects_copy.py
@@ -0,0 +1,12 @@
+
+rtsp = ['rtsp://user:pass@']
+
+objects = {
+    'id_object': {
+        'name': 'Name',
+        'ip': 'xx.xx.xx.xx',
+        'ports': ['11111', '22222'],
+        'rtsp': rtsp[0],
+    },
+}
+
