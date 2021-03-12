b39051e48bd1db80615694567b5f911478a26d97
vigi4 <6689363@gmail.com>
2021-03-12 19:12:09 +0300

    change objects_template

--- cam_objects_template.py
+++ cam_objects_template.py
@@ -1,26 +1,23 @@
-
-rtsp = {
-    'rtsp1': 'rtsp://user:user@{}:{}',
-    'rtsp2': 'rtsp://{}:{}/1/h264major',
-    'rtsp3': '"rtsp://{}:{}/user=user&password=pass&channel=1&stream=0?.sdp"',
-    'rtsp4': 'rtsp://admin:admin@{}:{}',
-    'rtsp5': 'rtsp://user:user@{}:{}',
-}
+rtsp1 = 'rtsp://user:user@{}:{}'
+rtsp2 = 'rtsp://{}:{}/1/h264major'
+rtsp3 = '"rtsp://{}:{}/user=user&password=pass&channel=1&stream=0?.sdp"'
+rtsp4 = 'rtsp://admin:admin@{}:{}'
+rtsp5 = 'rtsp://user:user@{}:{}'
 
 objects = {
     'xxxx': {
         'name': 'xxxxx',
         'ip': 'xxxxx',
         'ports': ['xxxx', 'xxxx'],
-        'rtsp': rtsp['rtsp2']
+        'rtsp': rtsp1
     },
-
+    # Шаблон, если у объекта некольких rtsp ссылок
     'xxx': {
         'name': 'xxxxx',
         'ip': 'xxxxxx',
         'ports': ['xxxxxx', 'xxxxx'],
         'port2': ['xxxx'],  # Порты для rtsp2
-        'rtsp': rtsp['rtsp3'],
-        'rtsp2': rtsp['rtsp1']
+        'rtsp': rtsp2,
+        'rtsp2': rtsp3
     },
 }
