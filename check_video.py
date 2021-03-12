8a5a7ec205f0c7d631c2f47b8023ec9150440df9
vigi4 <6689363@gmail.com>
2021-03-12 23:17:59 +0300

    fix errors id_objects, fix err_port

--- check_video.py
+++ check_video.py
@@ -14,24 +14,21 @@ def check_err_id_object():
 
 
 def check_err_port(id):
-    port2 = cam.objects[id].get('port2')
-
     while True:
-        port = input('{}{} Введите порт: '.format(cam.objects[id]['ports'], port2))
-
-        if port in cam.objects[id]['ports']:
-            return port
-        elif port in cam.objects[id]['port2']:
-            return port
-        elif port.lower() == 'q':
-            check_err_id_object()
-        else:
+        port = input('{}{} Введите порт: '.format(cam.objects[id]['ports'], cam.objects[id].get('port2')))
+        try:
+            if port in cam.objects[id]['ports']:
+                return port
+            elif port.lower() == 'q':
+                main()
+            elif port in cam.objects[id].get('port2'):
+                return port
+        except:
             print('Ошибка, некорректный порт')
 
 
 def check_video(id, port):
-
-    if port in cam.objects[id]['port2']:
+    if port is cam.objects[id].get('port2'):
         link = cam.objects[id]['rtsp2'].format(cam.objects[id]['ip'], port)
         vlc = f"vlc {link}"
 
@@ -42,8 +39,13 @@ def check_video(id, port):
     os.system(vlc)
 
 
-if __name__ == '__main__':
+def main():
     id_object = check_err_id_object()
+
     while True:
         port = check_err_port(id_object)
         check_video(id_object, port)
+
+
+if __name__ == '__main__':
+    main()
